6c48db6648104192455a2596972c9f5791962505
farhadmiacu <farhadmia.cu@gmail.com>
2025-09-05 06:26:35 +0600

    5th sep

--- routes/admin_farhad.php
+++ routes/admin_farhad.php
@@ -38,5 +38,6 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
     Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
     Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    
 
 });
