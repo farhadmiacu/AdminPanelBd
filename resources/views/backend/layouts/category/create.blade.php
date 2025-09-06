393b39432606da4f5426b96f31fcfc3c32c55386
farhadmiacu <farhadmia.cu@gmail.com>
2025-09-06 21:50:49 +0600

    sept 06

--- resources/views/backend/layouts/category/create.blade.php
+++ resources/views/backend/layouts/category/create.blade.php
@@ -107,7 +107,7 @@
             // Initialize Dropify
             $('.dropify').dropify({
                 messages: {
-                    'default': 'Drag and drop a file here or click',
+                    'default': 'Drag and drop here or click',
                     'replace': 'Drag and drop or click to replace',
                     'remove': 'Remove file',
                     'error': 'Ooops! something went wrong.'
