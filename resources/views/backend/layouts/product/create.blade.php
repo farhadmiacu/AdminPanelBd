3963e7a65acec66cdc707356d59c038da1fca07b
farhadmiacu <farhadmia.cu@gmail.com>
2025-09-11 00:40:05 +0600

    ckeditor image upload for long text

--- resources/views/backend/layouts/product/create.blade.php
+++ resources/views/backend/layouts/product/create.blade.php
@@ -187,11 +187,47 @@
             document.getElementById('slug').value = slug;
         });
     </script>
-    <script>
+    {{-- <script>
         ClassicEditor
             .create(document.querySelector('#long_description'))
             .catch(error => {
                 console.error(error);
             });
+    </script> --}}
+    <script>
+        ClassicEditor
+            .create(document.querySelector('#long_description'), {
+                extraPlugins: [MyCustomBase64UploadAdapterPlugin]
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        // Base64 Upload Adapter Plugin
+        function MyCustomBase64UploadAdapterPlugin(editor) {
+            editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+                return new Base64UploadAdapter(loader);
+            };
+        }
+
+        class Base64UploadAdapter {
+            constructor(loader) {
+                this.loader = loader;
+            }
+
+            upload() {
+                return this.loader.file
+                    .then(file => new Promise((resolve, reject) => {
+                        const reader = new FileReader();
+                        reader.onload = () => resolve({
+                            default: reader.result
+                        });
+                        reader.onerror = err => reject(err);
+                        reader.readAsDataURL(file);
+                    }));
+            }
+
+            abort() {}
+        }
     </script>
 @endpush
